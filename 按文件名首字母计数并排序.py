1912e4f6a845b61e9acf62f58c8bf645c4bdf4c2
windy003 <928920471@qq.com>
2020-11-14 13:51:49 +0800

    Add files via upload

---
+++ 按文件名首字母计数并排序.py
@@ -0,0 +1,23 @@
+import os
+
+文件路径=input("请输入文件路径:(注意首字母的大小写都会被记录!)")
+
+文件名列表=os.listdir(文件路径)
+
+字母列表=[]
+
+for letter in range(65,91):
+     字母列表.append(chr(letter))
+
+名字列表={}
+for b in 字母列表:
+        名字列表["%s开头的文件或文件夹个数" % b] = 0
+
+for a in 文件名列表:
+    for b in 字母列表:
+        if a.upper().startswith(b):
+            名字列表["%s开头的文件或文件夹个数" % b]+=1
+
+元组列表=zip(名字列表.values(),名字列表.keys())
+print(list(sorted(元组列表,reverse=True)))
+
