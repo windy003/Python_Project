94f1b276dbc41bb9928363b634a90b6aaa83ba91
windy003 <928920471@qq.com>
2020-12-04 16:16:50 +0800

    Create 一个简单的pygame界面.py

---
+++ pygame_Projects/一个简单的pygame界面.py
@@ -0,0 +1,15 @@
+import pygame
+from pygame.locals import *
+import sys
+def hello_world():
+   pygame.init()
+   pygame.display.set_mode((640,480))
+   pygame.display.set_caption("hello world")
+   while True:
+       for event in pygame.event.get():
+           if event.type==QUIT:
+              pygame.quit()
+               sys.exit()
+           pygame.display.update()
+if __name__=="__main__":
+   hello_world()
