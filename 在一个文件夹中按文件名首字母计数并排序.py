ca8a457dabe1d4e8d529a7a4a6ab50dc3949ab4c
windy003 <928920471@qq.com>
2021-09-27 11:48:03 +0800

    Update 在一个文件夹中按文件名首字母计数并排序.py

--- 在一个文件夹中按文件名首字母计数并排序.py
+++ 在一个文件夹中按文件名首字母计数并排序.py
@@ -9,16 +9,17 @@ import os
 for letter in range(65,91):
      字母列表.append(chr(letter))
 
-名字列表={}
+名字字典={}
 for b in 字母列表:
-        名字列表["%s开头的文件或文件夹个数" % b] = 0
+        名字字典["%s开头的文件或文件夹个数" % b] = 0
 
 for a in 文件名列表:
     for b in 字母列表:
         if a.upper().startswith(b):
-            名字列表["%s开头的文件或文件夹个数" % b]+=1
+            名字字典["%s开头的文件或文件夹个数" % b]+=1
 
-元组列表=zip(名字列表.values(),名字列表.keys())
+元组列表=zip(名字字典.values(),名字字典.keys())
 # 元组列表是一个zip对象
+print("共%s个"  % len(文件名列表))
 print(sorted(元组列表,reverse=True))
 #sorted 可以对所有可迭代的对象进行排序操作，返回的是一个列表
