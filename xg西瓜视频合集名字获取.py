e9965d2f6fb61bfd110e55cb7c9e446971f94709
windy003 <928920471@qq.com>
2022-01-05 19:56:17 +0800

    Add files via upload

--- xg西瓜视频合集名字获取.py
+++ xg西瓜视频合集名字获取.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
-import re
 
 
 
-with open ('./网页源码.html','r',encoding='UTF-8') as 源文件变量:
+with open ('./网页源码.html','r',encoding='UTF-8',errors='ignore') as 源文件变量:
         源文件数据=源文件变量.read()
 bs4解析出来的数据 = BeautifulSoup(源文件数据, "html.parser")
 
