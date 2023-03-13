688159f21078b185221edd540751c18b28422ddd
windy003 <928920471@qq.com>
2023-03-13 20:16:14 +0800

    Update title用python获取网页title标签内容.py

--- title用python获取网页title标签内容.py
+++ title用python获取网页title标签内容.py
@@ -7,9 +7,21 @@ response = requests.get(url)
 
 # 解析HTML
 soup = BeautifulSoup(response.content, 'html.parser')
+meta_tag = soup.find("meta", {"name":"description"})
 
 
-soup = BeautifulSoup(response.content, 'html.parser')
-meta_tag = soup.find("meta", {"name": "description"})
-description = meta_tag["content"]
-print(description)
+# description = meta_tag["content"]
+# print(description)
+
+
+if meta_tag:
+    description = meta_tag["content"]
+    print(description)
+else:
+    print("该网页没有description信息.")
+
+
+
+
+
+
